0d438ba35e09e2eb188b18e26a1e63ecd6cdb1ad
Aurora-NB <772335766@qq.com>
2021-01-07 20:16:27 +0800

    更新了英文语言包

--- litemall-wx/languageMap/English.js
+++ litemall-wx/languageMap/English.js
@@ -1,6 +1,264 @@
 module.exports = {
-  '项目名称:':'project name:',
-  '项目地址:':'project address:',
-  '电话号码:':'phone number:',
-  'QQ交流群:':'QQ:',
+  //about
+  '项目名称:':'project Name:',
+  '项目地址:':'project address: ',
+  '电话号码:':'phone number: ',
+  'QQ交流群:':'qq communication group: ',
+
+  //auth/login
+  '微信登录':'wechat Login',
+  '微信登录失败':'wechat login failed',
+
+  //brand
+  '元起':'yuan',
+
+  //cart
+  '30天无忧退货':'30 day worry free return',
+  '48小时快速退款':'48 hour quick refund',
+  '满88元免邮费':'no postage over 88 yuan',
+  '空空如也':'empty',
+  '全选（':'select all(',
+  '编辑':'edit ',
+  '完成':'finish ',
+  '删除':'delete',
+  '下单':'place an order ',
+  '还没有登录':'not logged in yet ',
+  '去登录':'log in',
+  '已选择:':'selected:',
+
+
+  //catalog
+  '商品搜索, 共':'product search, total',
+  '款好物':'goods',
+  '分类':'classification ',
+
+  //checkout
+  '默认':'default',
+  '还没有收货地址，去添加':'No receiving address, go to add',
+  '如需要，请输入留言':'If necessary, please enter a message',
+  '商品合计':'total goods',
+  '运费':'freight ',
+  '实付':'paid in',
+
+  //comment
+  '全部':'All ',
+  '有图':'with graph ',
+  '商家回复':'business reply ',
+
+  //commentPost
+  '评分':'score',
+  '留言经过筛选后，对所有人可见':'after filtering, messages will be visible to all ',
+  '图片上传':'picture upload',
+  '取消':'cancel ',
+  '发表':'publish ',
+
+  //goods
+  '分享':'share',
+  '分享给好友':'share with friends',
+  '生成海报':'generate Poster ',
+  '原价':'original price',
+  '现价':'current price',
+  '评价':'evaluation ',
+  '查看全部':'view all',
+  '商品参数':'commodity parameters',
+  '常见问题':'frequently asked questions',
+  '大家都在看':'everybody\'s watching',
+  '价格':'price ',
+  '团购立减':'group buying and reducing',
+  '人':'People ',
+  '数量':'quantity ',
+  '加入购物车':'add shopping cart',
+  '参加团购':'group buying',
+  '立即购买':'buy now',
+  '商品已售空':'goods sold out',
+
+  //groupon/grouponDerail
+  '开团还缺':'Lack of',
+  '邀请参团':'invite to join the Group',
+  '参与团购':'participating in group buying ',
+  '查看全部':'view all',
+  '商品信息':'commodity information',
+  '商品合计：':'total commodity:',
+  '商品运费：':'commodity freight: ',
+  '商品实付：':'paid in goods: ',
+
+  //groupon/grouponList
+  '人成团':'people form a group',
+  '有效期至':'valid until ',
+  '现价：':'current price:',
+  '团购价：':'group purchase price:',
+  '上一页':'last page',
+  '下一页':'next page',
+
+  //groupon/myGroupon
+  '发起的团购':'group buying initiated',
+  '参加的团购':'group buying participated',
+  '尚未参加任何团购':'have not participated in any group purchase',
+  '开团中':'opening',
+  '开团成功':'successful opening',
+  '开团失败':'failed to open the Group',
+  '发起':'initiate ',
+  '订单编号：':'order number: ',
+  '团购立减：':'group purchase deduction: ',
+  '团购要求：':'group purchase requirements: ',
+  '当前参团：':'current delegation:',
+  '共':'total',
+  '件商品':'items',
+  '实付：':'paid in: ',
+
+  //help
+  '上一页':'last page ',
+  '下一页':'next page ',
+
+  //hotGoods
+  '综合':'synthesis',
+  '价格':'price ',
+  '分类':'classification ',
+
+  //index
+  '商品搜索, 共 ':'product search of  ',
+  '人成团':'people form a group',
+  '有效期至':'valid until',
+  '品牌制造商直供':'direct supply by brand manufacturer',
+  '元起':'yuan',
+  '周一周四 · 新品首发':'New product launch on Monday and Thursday ',
+  '人气推荐':'popular recommendation',
+  '专题精选':'special selection',
+  '更多':'more',
+  '好物 >':'good things >',
+
+  
+  //payResult
+  '付款成功':'Payment successful',
+  '查看订单':'check order',
+  '继续逛':'Continue shopping',
+  '付款失败':'Payment Fail',
+  '请在':'please at',
+  '半小时':'half an hour',
+  '内完成付款':'Complete payment within',
+  '否则订单将会被系统取消':'Otherwise the order will be cancelled by the system',
+  '重新付款':'repay',
+  '取消':'cancel',
+
+  //search
+  '历史记录':'history record',
+  '热门搜索':'popular searches',
+  '您寻找的商品还未上架':'The product you are looking for is not yet available',
+
+  //topicComment
+  '全部':'全部',
+  '有图':'There are pictures',
+
+  //topicCommentPost
+  '评分':'score',
+  '留言经过筛选后，对所有人可见':'After the message is filtered, it is visible to everyone',
+  '发表':'Publish',
+
+  //topicDetail
+  '专题商品':'Special products',
+  '精选留言':'Featured Message',
+  '查看更多':'see more',
+  '等你来留言':'Waiting for you to leave a message',
+  '专题推荐':'Recommended topics',
+
+
+  //ucenter/address
+  '默认':'default',
+  '收货地址还没有':'No shipping address yet',
+  '新建':'new',
+
+  //ucenter/adressAdd
+  '姓名':'name',
+  '手机号码':'mobile phone number',
+  '省份、城市、区县':'provinces, cities, districts and counties',
+  '详细地址, 如街道、楼盘号等':'detailed address, such as street, building number, etc.',
+  '设为默认地址':'set as the default address',
+  '保存':'save',
+  '确定':'determine',
+
+  //ucenter/aftersafe
+  '退款商品':'refund goods',
+  '订单明细':'order Details',
+  '商品总价':'total commodity price',
+  '优惠':'discount',
+  '元':'yuan',
+  '订单实付':'order actually paid',
+  '操作':'operating',
+  '退款类型':'type of refund',
+  '请选择':'please choose',
+  '退款原因':'reason for return',
+  '请输入':'please enter',
+  '退款金额':'refund amount',
+  '不可修改，即订单实付- 运费':'Cannot be modified, that is, the actual payment of the order minus the freight',
+  '退款说明':'refund instructions',
+  '上传凭证':'upload certificate',
+  '最多上传三张':'upload up to three',
+  '申请售后':'Apply for after-sales',
+
+  //ucenter/aftersaleDetvail
+  '退款明细':'Refund details',
+  '售后状态':'After-sales status',
+  '服务编号':'Service number',
+  '即订单实付- 运费':'That is, the actual payment of the order minus the freight',
+
+  //ucenter/aftersaleList
+  '申请中':'Applying',
+  '处理中':'Processing',
+  '已完成':'completed',
+  '已拒绝':'rejected',
+  '还没有呢':'Not yet',
+  '售后编号：':'After-sale number:',
+  '申请退款金额：':'Request refund amount:',
+
+  //ucenter/collect
+  '还没有收藏':'No collection yet',
+
+  //ucenter/feedback
+  '对我们网站、商品、服务，你还有什么建议吗？你还希望在商城上买到什么？请告诉我们...':
+  'Do you have any suggestions for our website, products, and services? What else do you want to buy on the mall? please tell us',
+  '方便我们与你联系':'so that we can contact you',
+  '提交':'submit',
+
+  //ucenter/footprint
+  '没有浏览足迹':'No browsing footprint',
+
+  //ucenter/index
+  '我的订单':'My Order',
+  '待付款':'Pending payment',
+  '待发货':'to be delivered',
+  '待收货':'To be received',
+  '待评价':'comment',
+  '售后':'After sale',
+  '核心服务':'core services',
+  '商品收藏':'Commodity collection',
+  '浏览足迹':'Browse footprint',
+  '我的拼团':'My join',
+  '地址管理':'Address management',
+  '切换语言':'Switch language',
+  '取消':'cancel',
+  '必备工具':'Essential tools',
+  '帮助中心':'Help center',
+  '意见反馈':'Feedback',
+  '联系客服':'Contact Customer Service',
+  '关于我们':'about us',
+  '退出登录':'sign out',
+
+  //uncenter/order
+  '还没有任何订单呢':'No order yet',
+
+  //ucenter/orderDetail
+  '下单时间：':'order time:',
+  '订单留言：':'Order Message:',
+  '取消订单':'cancel order',
+  '去付款':'To pay',
+  '确认收货':'Confirm receipt',
+  '删除订单':'Delete order',
+  '申请退款':'Request a refund',
+  '去评价':'To evaluate',
+  '再次购买':'buy again',
+  '运费：':'Freight:',
+  '优惠：':'Offer:',
+  '实付：':'Actually paid:',
+  '快递公司：':'Courier company:',
+  '物流单号：':'shipment number:'
 }
